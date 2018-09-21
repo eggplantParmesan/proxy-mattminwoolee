fe72896f1a9fd4847305a15ec6e4f0561b864e76
Matthew Lee <Mattminwoolee@Matthews-MacBook-Pro-3.local>
2018-09-20 19:15:47 -0700

    Refactored proxy.

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+// const request = require('request'); 
 const app = express();
 const port = process.env.PORT || 3000;
+const fetch = require('node-fetch');
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/checkout/:productId', (req, res) => {
+  fetch(`http://localhost:7777/checkout/${req.params.productId}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then(json => res.send(json));
+});
+
+app.get('/api/reviews/:productId', (req, res) => {
+  fetch(`http://localhost:1337/reviews/${req.params.productId}`)
+    .then((res) => {
+      return res.json();
+    })
+    .then(json => res.send(json));
+});
+
+app.get('/api/helpful/:productId', (req, res) => {
+  fetch(`http://localhost:1337/helpful/${req.params.productId}`)
+    .then((res) => {
+      // do nothing
+    })
+    .then(json => res.status(202).send());
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
