c2480a7caabd2f69a7a84095101593d4f7b37da6
Matthew Lee <Mattminwoolee@Matthews-MacBook-Pro-3.local>
2018-09-22 17:00:54 -0700

    Implemented Routing to deployed services

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7778;
 const fetch = require('node-fetch');
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/checkout/:productId', (req, res) => {
-  fetch(`http://localhost:7777/checkout/${req.params.productId}`)
+  fetch(`http://ec2-18-224-5-50.us-east-2.compute.amazonaws.com:80/checkout/${req.params.productId}`)
     .then((res) => {
       return res.json();
     })
@@ -17,7 +17,7 @@ app.get('/api/checkout/:productId', (req, res) => {
 });
 
 app.get('/api/reviews/:productId', (req, res) => {
-  fetch(`http://localhost:1337/reviews/${req.params.productId}`)
+  fetch(`http://ec2-54-193-70-8.us-west-1.compute.amazonaws.com:80/reviews/${req.params.productId}`)
     .then((res) => {
       return res.json();
     })
@@ -25,7 +25,8 @@ app.get('/api/reviews/:productId', (req, res) => {
 });
 
 app.get('/api/helpful/:productId', (req, res) => {
-  fetch(`http://localhost:1337/helpful/${req.params.productId}`)
+  fetch(`Alex Vincent-Hill [3:53 PM]
+  http://ec2-54-193-70-8.us-west-1.compute.amazonaws.com:80/helpful/${req.params.productId}`)
     .then((res) => {
       // do nothing
     })
@@ -33,7 +34,7 @@ app.get('/api/helpful/:productId', (req, res) => {
 });
 
 app.get('/api/products/', (req, res) => {
-  fetch(`http://localhost:9001/get?id=${req.query.id}`)
+  fetch(`http://ec2-13-57-32-246.us-west-1.compute.amazonaws.com:80/get?id=${req.query.id}`)
     .then((response) => {
       return response.json();
     }).then((json) => {
@@ -42,7 +43,7 @@ app.get('/api/products/', (req, res) => {
 });
 
 app.get('/api/relatedItems', (req, res) => {
-  fetch(`http://localhost:4043/product?id=${req.query.id}`)
+  fetch(`http://ec2-13-57-246-165.us-west-1.compute.amazonaws.com:3036/product?id=${req.query.id}`)
     .then((res) => {
       return res.json();
     })
