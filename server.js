ed00551ec1d5ee4886a826f92fff34457450a2fa
Richard Hahn <rhahn07@yahoo.com>
2018-10-09 12:37:33 -0700

    Initial implemenation of proxy, VSCode is now my text editor

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('newrelic')
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -8,46 +9,46 @@ const fetch = require('node-fetch');
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/checkout/:productId', (req, res) => {
-  fetch(`http://localhost:7777/checkout/${req.params.productId}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then(json => res.send(json));
-});
+// app.get('/api/checkout/:productId', (req, res) => {
+//   fetch(`http://localhost:7777/checkout/${req.params.productId}`)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then(json => res.send(json));
+// });
 
-app.get('/api/reviews/:productId', (req, res) => {
-  fetch(`http://localhost:1337/reviews/${req.params.productId}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then(json => res.send(json));
-});
+// app.get('/api/reviews/:productId', (req, res) => {
+//   fetch(`http://localhost:1337/reviews/${req.params.productId}`)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then(json => res.send(json));
+// });
 
 app.get('/api/helpful/:productId', (req, res) => {
-  fetch(`http://localhost:1337/helpful/${req.params.productId}`)
+  fetch(`http://localhost:3306/cruddy/${req.params.productId}`)
     .then((res) => {
       // do nothing
     })
     .then(json => res.status(202).send());
 });
 
-app.get('/api/products/', (req, res) => {
-  fetch(`http://localhost:9001/get?id=${req.query.id}`)
-    .then((response) => {
-      return response.json();
-    }).then((json) => {
-      res.send(json);
-    });
-});
+// app.get('/api/products/', (req, res) => {
+//   fetch(`http://localhost:9001/get?id=${req.query.id}`)
+//     .then((response) => {
+//       return response.json();
+//     }).then((json) => {
+//       res.send(json);
+//     });
+// });
 
-app.get('/api/relatedItems', (req, res) => {
-  fetch(`http://localhost:4043/product?id=${req.query.id}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then(json => res.send(JSON.stringify(json)));
-});
+// app.get('/api/relatedItems', (req, res) => {
+//   fetch(`http://localhost:4043/product?id=${req.query.id}`)
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then(json => res.send(JSON.stringify(json)));
+// });
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
